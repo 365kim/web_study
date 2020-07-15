dfcf8ff5c628bae646df91f3c78f48d3c0da2846
365kim <60066472+365kim@users.noreply.github.com>
2020-07-15 13:52:14 +0900

    2020. 07. 15. Updated Navbar

--- Practice/src/components/Navbar.js
+++ Practice/src/components/Navbar.js
@@ -1,97 +1,62 @@
-import React from 'react';
-import {
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarNav,
-    MDBNavbarToggler,
-    MDBCollapse,
-    MDBNavItem,
-    MDBNavLink,
-    MDBMask,
-    MDBView,
-    MDBDropdown,
-    MDBDropdownToggle,
-    MDBDropdownMenu,
-    MDBDropdownItem,
-    MDBIcon
-} from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
-import "../index.css";
-
-class FullPageNavbar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collapse: false,
-            isWideEnough: false,
-        };
-        this.onClick = this.onClick.bind(this);
-    }
-
-    onClick() {
-        this.setState({
-            collapse: !this.state.collapse,
-        });
-    }
-
-    render() {
-        return (
-            <div id="nav">
-                <header>
-                    <Router>
-                        <MDBNavbar color="bg-dark" fixed="top" dark expand="md" scrolling transparent>
-                            <MDBNavbarBrand id="brand" href="/">
-                                예발자닷컴
-                            </MDBNavbarBrand>
-                            {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
-                            <MDBCollapse isOpen={this.state.collapse} navbar>
-                                <MDBNavbarNav left>
-                                    <MDBNavItem className="nav-item-text">
-                                        <MDBNavLink to="/yebaljadotcom.html">예발자란?</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem className="nav-item-text">
-                                        <MDBNavLink to="#">부트캠프비교</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem className="nav-item-text">
-                                        <MDBDropdown>
-                                            <MDBDropdownToggle nav caret color="secondary">
-                                                부트캠프상세
-                                            </MDBDropdownToggle>
-                                            <MDBDropdownMenu color="secondary">
-                                                <MDBDropdownItem>42서울</MDBDropdownItem>
-                                                <MDBDropdownItem>SSAFY</MDBDropdownItem>
-                                                <MDBDropdownItem>부스트캠프</MDBDropdownItem>
-                                                <MDBDropdownItem>멋쟁이사자처럼</MDBDropdownItem>
-                                                <MDBDropdownItem>SW마에스트로</MDBDropdownItem>
-                                                <MDBDropdownItem divider />
-                                                <MDBDropdownItem>연간일정</MDBDropdownItem>
-                                            </MDBDropdownMenu>
-                                        </MDBDropdown>
-                                    </MDBNavItem>
-                                    <MDBNavItem className="nav-item-text">
-                                        <MDBNavLink to="#">컨퍼런스</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem className="nav-item-text">
-                                        <MDBNavLink to="#">커뮤니티</MDBNavLink>
-                                    </MDBNavItem>
-                                </MDBNavbarNav>
-                            </MDBCollapse>
-                        </MDBNavbar>
-                    </Router>
+import styles from './NavBar.module.css'
+import Link from 'next/link';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import classNames from 'classnames';
+import { useRouter } from "next/router";
 
 
-                    <MDBView src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=763&q=80">
-                        <MDBMask id="main-text" overlay="purple-light" className="flex-center flex-column text-white text-center">
-                            <h1>'예비개발자'에서&nbsp;&nbsp;'주니어개발자'로</h1>
-                            <br />
-                            <h4>개발자 지망생을 위한 최신 정보를 확인하세요!</h4>
-                        </MDBMask>
-                    </MDBView>
+const NavBar = () => {
 
-                </header>
-            </div>
-        );
-    }
-}
+    const router = useRouter();
 
-export default FullPageNavbar;
+    return (
+        <div>
+            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" className={styles.navbarBg}>
+                <Navbar.Brand></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Link href="/ftseoul">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/ftseoul" ? "active" : ""]: true})}>
+                                42서울</a>
+                        </Link>
+                        <Link href="/ssafy">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/ssafy" ? "active" : ""]: true})}>
+                                SSAFY</a>
+                        </Link>
+                        <Link href="/boostcamp">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/boostcamp" ? "active" : ""]: true})}>
+                                부스트캠프</a>
+                        </Link>
+                        <Link href="/soma">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/soma" ? "active" : ""]: true})}>
+                                SOMA</a>
+                        </Link>
+                        <Link href="/woowa">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/woowa" ? "active" : ""]: true})}>
+                                우아한테크코스</a>
+                        </Link>
+                        <Link href="/likelion">
+                            <a className={classNames({["nav-link"]: true,
+                                [router.pathname == "/likelion" ? "active" : ""]: true})}>
+                                멋쟁이사자처럼</a>
+                        </Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="/sign-in">로그인</Nav.Link>
+                        <Nav.Link href="/sign-up">회원가입</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
